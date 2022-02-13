d2b8f1490e54e6deaf65042afe1d175cae5b972b
vkrassova <v.krassova@yandex.ru>
2022-02-13 17:15:05 +0300

    добавляет компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppProps = {
+  movieInfo: {
+    name: string,
+    genre: string,
+    released: number,
+  }
+};
+
+function App({movieInfo}: AppProps): JSX.Element {
+  return <MainPage name={movieInfo.name} genre={movieInfo.genre} released={movieInfo.released} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const MainPageProps = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'drama',
+  released: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App movieInfo = {MainPageProps} />
   </React.StrictMode>,
   document.getElementById('root'));
+
